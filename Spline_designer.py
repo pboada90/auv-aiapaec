9d2915b85650367e8906fdc6d5f0168c43293756
pboada90 <pedro.boada@pucp.pe>
2024-10-21 19:41:52 -0500

    docs: update folders and files

--- Spline_designer.py
+++ Spline_designer.py
@@ -4,13 +4,13 @@ import ezdxf
 a = 300
 b = 800
 c = 300
-d = 200
+d = 180
 
 l = a+b+c
 n = 2.5
 
-alpha = 30
-alpha = alpha * np.pi / 180
+theta = 30
+theta = theta * np.pi / 180
 
 x = np.linspace(0, l, 100)
 body = np.zeros((len(x), 3))
@@ -21,11 +21,9 @@ for count, element in enumerate(x):
     if (element <= (a+b)) & (element > a):
         value = d*0.5
     if (element <= l) & (element > (a+b)):
-        value = 0.5*d - (3*d/(2*c*c)-np.tan(alpha)/c)*(element-a-b)*(element-a-b) + (
-            d/(c*c*c)-np.tan(alpha)/(c*c))*(element-a-b)*(element-a-b)*(element-a-b)
-        #print(value)
+        value = 0.5*d - (3*d/(2*c*c)-np.tan(theta)/c)*(element-a-b)*(element-a-b) + (
+            d/(c*c*c)-np.tan(theta)/(c*c))*(element-a-b)*(element-a-b)*(element-a-b)
     body[count] = np.array([x[count], value, 0])
-
 
 doc=ezdxf.new('R2010')
 msp=doc.modelspace()
